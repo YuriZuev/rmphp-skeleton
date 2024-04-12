9abb4d4d6b1450eed7d448c75503bd779e66fee2
User <y_zuev@mail.ru>
2024-04-13 00:39:19 +0300

    20240413#2

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ $response = $app->handler($request, (new Response())->withHeader("Content-Type",
 (new ResponseEmitter())->emit($response);
 
 
-if((getenv("APP_MODE") == 'DEV' && $response->getStatusCode() !== 200) || in_array("Dev", $response->getHeader("App-Mode"))){
+if(($response->getStatusCode() !== 200 && getenv("APP_MODE") == 'DEV') || in_array("Dev", $response->getHeader("App-Mode"))){
 	$app->syslogger()->dump("Response", $response);
 	addShutdownInfo($app->syslogger()->getLogs());
 }
