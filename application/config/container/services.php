b53c3f9815ec4d7acdd27e8afac41310327dc9ff
User <y_zuev@mail.ru>
2025-01-27 03:09:37 +0300

    20250127#1

--- application/config/container/services.php
+++ application/config/container/services.php
@@ -1,12 +1,10 @@
 <?php
 
-use Rmphp\Cache\Cache;
-use Rmphp\Cache\CacheInterface;
 use Rmphp\Storage\Mysql\MysqlStorage;
 use Rmphp\Storage\Mysql\MysqlStorageInterface;
 
 return [
 	MysqlStorageInterface::class => DI\create(MysqlStorage::class)->constructor(json_decode(getenv("MYSQL_PARAM"), true)),
-	CacheInterface::class => DI\create(Cache::class)->constructor("../var/cache"),
+	'App\Domain\Repository\*RepositoryInterface' => DI\autowire('App\Infrastructure\Repository\*Repository'),
 	'App\*\Domain\Repository\*RepositoryInterface' => DI\autowire('App\*\Infrastructure\Repository\*Repository'),
 ];
