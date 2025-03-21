c2a14307ae57a11dcd87e739153246bcab5f92f8
User <y_zuev@mail.ru>
2025-03-21 21:50:55 +0300

    20250321#1

--- config/container/services.php
+++ config/container/services.php
@@ -4,7 +4,7 @@ use Rmphp\Storage\Mysql\MysqlStorage;
 use Rmphp\Storage\Mysql\MysqlStorageInterface;
 
 return [
-	MysqlStorageInterface::class => DI\create(MysqlStorage::class)->constructor(json_decode(getenv("MYSQL_PARAM"), true)),
+	MysqlStorageInterface::class => DI\create(MysqlStorage::class)->constructor(json_decode(getenv("MYSQL_PARAM"))),
 	'App\Domain\Repository\*RepositoryInterface' => DI\autowire('App\Infrastructure\Repository\*Repository'),
 	'App\*\Domain\Repository\*RepositoryInterface' => DI\autowire('App\*\Infrastructure\Repository\*Repository'),
 ];
